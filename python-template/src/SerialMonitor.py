5cec76d62c155874482566e10892ced828b511b0
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-05-28 15:15:05 +0530

    Add signal handling, remove unnecessary threading, handle multiple received lines (temp, to be fixed in firmware and removed)

--- python-template/src/SerialMonitor.py
+++ python-template/src/SerialMonitor.py
@@ -1,39 +1,41 @@
 import serial
-import time
-import threading
-from edit import encrypt, decrypt
+import sys
+import signal
+from edit import decrypt
 import json
 import edit
 
-serial_port = edit.serial_port  # Change this to your serial port
-baud_rate = 115200
-ser = serial.Serial(serial_port, baud_rate)
-
+def signal_handler(ser, sig, frame):
+    print("Signal received, stopping...")
+    ser.close()
+    sys.exit(0)
 
 def receive_messages(ser):
+    # Register signal handlers
+    signal.signal(signal.SIGINT, lambda sig, frame: signal_handler(ser, sig, frame))
+    signal.signal(signal.SIGTERM, lambda sig, frame: signal_handler(ser, sig, frame))
+
     while True:
-        if ser.in_waiting > 0:
-            serial_lines = ser.readline().decode('utf-8').strip().split("\n")
-            for line in serial_lines:
-                if line.startswith("Received"):
-                    # Remove "Received " from the message
-                    json_obj_dump = line.replace("Received ", "")
-                    print(f"Received message: {json_obj_dump}")
+        if ser.in_waiting:
+            line = ser.readline().decode('utf-8').strip()
+            if line.startswith("Received"):
+                # Split at received. 0th index is empty, 1st index is the message. 2nd index is light up instruction. Ignore.
+                message_payload = line.split("Received ")[1]
+                try:
+                    json_message = json.loads(message_payload)
+                    if 'msg' in json_message:
+                        decrypted_message = decrypt(json_message['msg'])
+                        print(f"Decrypted message: {decrypted_message}")
+                    #else:
+                        # Control flow should never reach here
+                        # print(f"DEBUG, This is light up instr: {json_message}")
+                except json.JSONDecodeError as e:
+                    print(f"{e}")
+            else:
+                print(f"{line}")
 
-                    try:
-                        json_message = json.loads(json_obj_dump)
-                        if 'msg' in json_message:
-                            decrypted_message = decrypt(json_message['msg'])
-                            print(f"Decrypted message: {decrypted_message}")
-                        else:
-                            print(f"DEBUG, This is light up instr: {json_message}")
-                    except json.JSONDecodeError as e:
-                        print(f"err is {e}")
-                else:
-                    print(f" \n")
-
-
-
-# Start the receive_messages thread
-receive_thread = threading.Thread(target=receive_messages, args=(ser,))
-receive_thread.start()
+if __name__ == "__main__":
+    serial_port = edit.serial_port  # Change this to your serial port
+    baud_rate = 115200
+    ser = serial.Serial(serial_port, baud_rate)
+    receive_messages(ser)
