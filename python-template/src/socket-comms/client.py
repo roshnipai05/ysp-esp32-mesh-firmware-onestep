5248984583b6308db5d62bbebc5d031221edb04d
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2024-06-07 17:19:00 +0530

    Fix client control flow: handle signals, exit cmd, and update tui

--- python-template/src/socket-comms/client.py
+++ python-template/src/socket-comms/client.py
@@ -1,24 +1,60 @@
+import os
+import signal
 import socket
+import sys
 
-def send_data(data):
-    host = '127.0.0.1'
-    port = 65432
+def trigger_exit():
+    os.kill(os.getpid(), signal.SIGINT)
+
+def signal_handler(sig, frame):
+    print('\nInitiating interface shutdown...')
+
+    # init server exit before shutting down
+    send_data('exit')
+    sys.exit(0)
+
+def check_server(host, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(2)
+            s.connect((host, port))
+            s.close()
+        return True
+    except socket.error as e:
+        # print(f"Server not available: {e}")
+        return False
+
+def send_data(data, host='127.0.0.1', port=65432):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
             s.sendall(data.encode())
-            print("Data sent")
     except ConnectionError as e:
-        print(f"Failed to connect or send datea: {e}")
+        print(f"Failed to connect or send data: {e}")
 
 def main():
-    while True:
-        try:
-            data = input("Enter data to send: ")
+    host = '127.0.0.1'
+    port = 65432
+
+    # Register signal handlers
+    signal.signal(signal.SIGINT, lambda sig, frame: signal_handler(sig, frame))
+    signal.signal(signal.SIGTERM, lambda sig, frame: signal_handler(sig, frame))
+
+    if not check_server(host, port):
+        print('Server must be running before client starts.')
+        print('Run `python server.py` in a different terminal before running this script.')
+        sys.exit(1)
+
+    print('Command Interface initiated. Press CTRL+C or type "exit" to exit.')
+    try:
+        while True:
+            data = input('\nEnter a command\n> ')
+            if data.lower() == 'exit':
+                trigger_exit()
+                break
             send_data(data)
-        except KeyboardInterrupt:
-            print('\nClient interrupted by user')
-            break
+    finally:
+        print('Closing command interface')
 
 if __name__ == '__main__':
     main()
